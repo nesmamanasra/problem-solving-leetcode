59c6bf4f7b7605ba6583fd19111621d938981371
nesma manasarh <52491098+anaal@users.noreply.github.com>
2022-07-17 00:11:15 +0300

    solve this problem in the simplest way

--- roman-to-integer.js
+++ roman-to-integer.js
@@ -1,9 +1,17 @@
 let romanToInt = function(s) {
     const map = {I : 1, V : 5, X : 10, L : 50, C : 100, D : 500, M : 1000};
     let res = 0 ;
-    s.split('').forEach((num ,i) => {
-        if(map[num] < map[s[i+1]]) res += map[num]
-        else res += map[num]
-    });
-    return res;
+    for( let i=0; i< s.length; i++){
+        let one = map[s[i]];
+        let tow = map[s[i + 1]];
+        if(one < tow){
+            res += tow - one
+            //res = res + tow - one 
+            i++
+        }else{
+            // res = res + map[s[i]]
+            res += map[s[i]]
+        }
+    }
+    return res
 };
